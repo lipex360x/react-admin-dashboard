b11a5541ccc815ee82b359eb7dd7dba077449270
Felipe Bambirra <lipex360@gmail.com>
2022-11-13 21:22:59 -0300

    Contact page added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 
 // import { Bar } from './scenes/bar'
 // import { Calendar } from './scenes/Calendar'
-// import { Contacts } from './scenes/contacts'
+import { Contacts } from './scenes/contacts'
 import { Dashboard } from './scenes/dashboard'
 // import { FAQ } from './scenes/faq'
 // import { Form } from './scenes/form'
@@ -33,8 +33,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
